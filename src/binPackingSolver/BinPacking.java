c3597fd7510181b3177d96efb23da8adacad14ab
Schikocko <athnemesis@web.de>
2015-09-11 13:12:43 +0200

    facelift

--- src/binPackingSolver/BinPacking.java
+++ src/binPackingSolver/BinPacking.java
@@ -26,8 +26,19 @@ public interface BinPacking {
 	    */
 	public int binsUsed();
 	
-	
+	/**
+	 * writes in the file "bin_report and prints each bin with all objects contained and their weight  
+	 * aswell as the current load and the space left
+	 * 
+	 * @param Sting name give the name of the binbacking problem, to identify it
+	 */
 	public void printBin(String name);
 	
+	
+	/**
+	 * takes the given input and sorts in decreasing by objects weight, starting with the highest to the lowest 
+	 * @param ArrayList<BinObject> l ArrayList of the given objects for the problem
+	 * @return return a new sorted ArrayList<BinObject> that can be used as input for the solveBinPacking() method  
+	 */
 	public ArrayList<BinObject> sortDecreasing (ArrayList<BinObject> l);
 }
--- src/binPackingSolver/NextFit.java
+++ src/binPackingSolver/NextFit.java
@@ -156,6 +156,7 @@ public class NextFit implements BinPacking{
 		/**
 		 * takes the given input and sorts in decreasing by objects weight, starting with the highest to the lowest 
 		 * @param ArrayList<BinObject> l ArrayList of the given objects for the problem
+		 * @return return a new sorted ArrayList<BinObject> that can be used as input for the solveBinPacking() method  
 		 */
 		public ArrayList<BinObject> sortDecreasing(ArrayList<BinObject> l){
 		
